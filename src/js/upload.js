cf989979ff0be9033b6a775480f0997989785170
Lucas Alexander Floriani <lucas.floriani@bplus.com.br.local>
2018-03-22 17:25:02 -0300

    Added remove and addCard logic

--- src/js/upload.js
+++ src/js/upload.js
@@ -1,14 +1,15 @@
-import './../scss/main.scss';
+// import './../scss/main.scss';
 
 class FileUpload { /* eslint-disable-line */
   /**
    * Constructor of object FileUpload
    */
-  constructor(container, name, identifier, callback = false) {
+  constructor(container, name, identifier, addCallback = false, removeCallback = false) {
     this.container = document.querySelector(container);
     this.name = name;
     this.identifier = identifier;
-    this.callback = callback;
+    this.addCallback = addCallback;
+    this.removeCallback = removeCallback;
   }
 
   /**
@@ -21,13 +22,15 @@ class FileUpload { /* eslint-disable-line */
   /**
    * Return template of file upload field
    */
-  getFileTemplate() {
-    return `
-      <li class="image-preview">
-        <label for="${this.identifier}"></label>
-        <input type="file" name="${this.name}" id="${this.identifier}">
-      </li>
-    `;
+  addFileCard() {
+    const li = document.createElement('li');
+    li.classList.add('file-preview-card');
+
+    li.innerHTML = `<label for="${this.identifier}" class="file-preview-button file-preview-add"></label>
+                    <button type="button" class="file-preview-button file-preview-remove"></button>
+                    <input type="file" name="${this.name}" class="file-preview-hidden" id="${this.identifier}">`;
+
+    this.container.appendChild(li);
   }
 
   /**
@@ -36,16 +39,20 @@ class FileUpload { /* eslint-disable-line */
   setUploadEvents() {
     let childrens = Array.from(this.container.children);
     childrens.forEach((li) => {
-      let input = li.children[1];
-      input.addEventListener('change', this.changeFileEvent.bind(input, this.callback));
+      let input = li.querySelector('input.file-preview-hidden');
+      input.addEventListener('change', this.changeFileEvent.bind(this, input, this.addCallback));
+
+      let removeButton = li.querySelector('.file-preview-remove');
+      removeButton.addEventListener('click', this.removeFileEvent.bind(input, this.removeCallback));
     });
   }
 
   /**
    * Event added in function setUploadEvents
    */
-  changeFileEvent(callback) {
-    const files = this.files;
+  changeFileEvent(input, addCallback) {
+    const self = input;
+    const files = input.files;
 
     if (files.length > 0) {
       const file = files[0];
@@ -55,16 +62,35 @@ class FileUpload { /* eslint-disable-line */
         const loadedFile = e.target;
 
         if (file.type.match('image')) {
-          this.parentNode.style.backgroundImage = `url('${loadedFile.result}')`;
+          self.parentNode.style.backgroundImage = `url('${loadedFile.result}')`;
         }
       });
 
       // Read file
       reader.readAsDataURL(file);
 
-      if (callback) {
-        callback();
+      this.addFileCard();
+
+      if (addCallback) {
+        addCallback();
       }
+    }
+  }
+
+  /**
+   * Remove Event
+   */
+  removeFileEvent(removeCallback) {
+    const li = this.parentNode;
+
+    if (Array.from(li.children).length > 1) {
+      li.parentNode.removeChild(li);
+    } else {
+      this.value = '';
+    }
+
+    if (removeCallback) {
+      removeCallback();
     }
   }
 }
